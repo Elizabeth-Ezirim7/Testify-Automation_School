ba4acbc2262a9a67f8bf3f9f0fc11513e8b51d04
Elizabeth-Ezirim <leezwills@gmail.com>
2023-10-12 16:52:50 +0100

    added subtaction

--- test.py
+++ test.py
@@ -7,7 +7,11 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.addition(1, 2), 3, "Should be 3")
         self.assertEqual(main.addition(5, 5), 10, "Should be 10")
         self.assertEqual(main.addition(40, 20), 60, "Should be 60")
-        self.assertEqual(main.addition(-3, 2), -1, "Should be -1")
+        self.assertEqual(min.addition(-3, 2), -1, "Should be -1")
 
-        if __name__ == '__main__':
-            unittest.main()
+    def test_subtraction(self):
+        self.assertEqual(main.subtraction(5, 4), 1, "Should be 1")
+
+
+if __name__ == '__main__ ':        
+    unittest.main()
