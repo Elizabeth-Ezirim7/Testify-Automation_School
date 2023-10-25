23214bd1e47880597c3d29fdff89ae203d9ab8b2
Elizabeth-Ezirim <leezwills@gmail.com>
2023-10-25 12:29:09 +0100

    corrections

--- Module3/Module3A/task7.js
+++ Module3/Module3A/task7.js
@@ -5,11 +5,11 @@ const side2 = 4;
 const side3 = 5;
 
 if(side1 === side2 && side2 === side3){
-    console.log("Equilateral Triangle");
+    console.log("Equilateral triangle");
 }
 else if (side1 === side2 || side1 === side3 || side2 === side3){
-    console.log('Isoceles Triangle')
+    console.log('Isoceles triangle')
 } 
 else{
-    console.log('Scalene Triangle')
+    console.log('Scalene triangle')
 }
