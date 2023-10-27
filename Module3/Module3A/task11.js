dd4e2985a2b816e202c3e72d5925b1ea360074ff
Elizabeth-Ezirim <leezwills@gmail.com>
2023-10-27 09:16:03 +0100

    tasks

--- Module3/Module3A/task11.js
+++ Module3/Module3A/task11.js
@@ -1,8 +1,11 @@
 
 //Array of Top 5 friends
 
-/*const friends = ['Olamide', 'Precious', 'Aridunnu', 'Zainab', 'Chioma']
-console.log(friends);
-*/
+const friends = ["Olamide", "Precious", "Aridunnu", "Zainab", "Chioma"]
+
+console.log('These are my top 5 friends: '+friends );
+
+
+
 
 
