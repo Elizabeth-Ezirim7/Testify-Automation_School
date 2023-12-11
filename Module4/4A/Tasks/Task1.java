917936185b43e21a8c20f049faee6d9d919e13c0
Elizabeth-Ezirim <leezwills@gmail.com>
2023-12-11 12:34:02 +0100

    test

---
+++ Module4/4A/Tasks/Task1.java
@@ -0,0 +1,17 @@
+package Tasks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("This is Java Class");
+        System.out.println("This is the beginning of Java");
+        System.out.println("This is an interesting Class");
+        System.out.println("Welcome to Java training");
+        System.out.println("Be a pro");
+        System.out.println("Practice is the watch word");
+        System.out.println("This is Java Class");
+        System.out.println("This is Java class for beginners");
+        System.out.println("Make sure you submit your task");
+        System.out.println("Become a Java programmer");
+
+    }
+}
