1f718070920ce564c99d0412e24f259112994d80
Elizabeth-Ezirim <leezwills@gmail.com>
2023-12-14 22:21:55 +0100

    upddate2

---
+++ Module4/4A/Tasks/Task19/Task19d/Task19d1.java
@@ -0,0 +1,11 @@
+package Tasks.Task19.Task19d;
+
+public class Task19d1 {
+    String instanceMember = "Delta";
+
+    // Method to print the instance member and user-provided name
+    public void printName(String username) {
+        System.out.println("Instance member: " + instanceMember);
+        System.out.println("User-provided name: " + username);
+    }
+}
